b9d46d5c492eeb08312f853aeeb90cfbe4579eaa
nadiiiiia <nadia.hbr@gmail.com>
2018-07-10 09:38:58 +0100

    update ImageType

---
+++ src/AppBundle/Form/ImageType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+class ImageType extends AbstractType {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder->add('titre')
+                ->add('file', FileType::class, array('attr' => array(
+                        'accept' => 'image/*' // pour n'accepter que les images
+                    ),
+        ));
+    }
+
+/**
+     * {@inheritdoc}
+     */
+
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Image'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix() {
+        return 'appbundle_image';
+    }
+
+}
